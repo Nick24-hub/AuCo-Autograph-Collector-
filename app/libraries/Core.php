a48073b64c8d1e18f0636cf964cc5e3397bd050d
Alexandrescu Nicolae <nicu24dec.an@gmail.com>
2022-06-07 15:31:02 +0300

    small fix

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -17,14 +17,14 @@ class Core
     $url = $this->getUrl();
 
     // Look in BLL for first value
-    if ($url) {
-      if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
-        // If exists, set as controller
-        $this->currentController = ucwords($url[0]);
-        // Unset 0 Index
-        unset($url[0]);
-      }
+
+    if ($url && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+      // If exists, set as controller
+      $this->currentController = ucwords($url[0]);
+      // Unset 0 Index
+      unset($url[0]);
     }
+
 
     // Require the controller
     require_once '../app/controllers/' . $this->currentController . '.php';
--- app/views/includes/head.php
+++ app/views/includes/head.php
@@ -2,10 +2,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial scale=1.0">
     <title>AuCo (Autograph Collector)</title>
-    <link rel="stylesheet" href="../public/css/style.css">
-    <link rel="stylesheet" href="../public/css/account.css">
-    <link rel="stylesheet" href="../public/css/footer.css">
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style.css">
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/account.css">
+    <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/footer.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 
 </head>
-
